295fee8bf736fbbf878f8101b941a52e7110f7e6
Zehui <zy18763@nottingham.edu.cn>
2017-04-25 22:16:59 +0800

    modify generate.py resolve platform-dependent problem and strange character problem

--- tools/scripts_py/generate.py
+++ tools/scripts_py/generate.py
@@ -1,4 +1,7 @@
+#coding=utf-8
 import os, sys, shutil, string, re
+reload(sys) 
+sys.setdefaultencoding("utf-8")
 #this script is used to get all the files written by a certain author
 #you can do so by typing in the name of the author and run this script,
 #the author name is the third column of the metadata 
@@ -9,8 +12,8 @@ import os, sys, shutil, string, re
 #          then you can get a folder containing the corresponding files appearing in the "main directory" of CLEAR 
 
 # before running this script, please go through following points:
-# > this script is designed to be used on windows platform,
-#   if you want to run this script on ios, please try to change the directory form
+# > please note that if the authors' surnames contain æ, ø ,ë and å
+#	try to type in the author's name manually below instead of typing it in the terminal
 # > please don't change the directory structure of "CLEAR"
 #	Just keep it the same as you clone it - the change of the dir structure could 
 # 	lead to the failure of the scripts 
@@ -19,11 +22,11 @@ import os, sys, shutil, string, re
 # this function can copy all the files and put them in a new folder 
 def creat_corpus(corpus_name,file_list):
 	cwd=os.getcwd()
-	parent_path = os.path.dirname(cwd)
+	parent_path = os.path.dirname(os.path.dirname(cwd))
 	os.chdir(parent_path)
 	os.mkdir(corpus_name)
-	des_dir=parent_path	+"\\"+ corpus_name
-	working_dir=parent_path + "\\"+ "data_adl"	
+	des_dir=os.path.join(parent_path,corpus_name)
+	working_dir=os.path.join(parent_path,"ADL","plain")	
 	os.chdir(working_dir)
 	for file_name in file_list:
 		shutil.copy(file_name,des_dir)
@@ -34,8 +37,8 @@ def creat_corpus(corpus_name,file_list):
 def	creat_file_list(author_name):
 	file_list=['non']
 	cwd=os.getcwd()
-	parent_path = os.path.dirname(cwd)
-	des_pos=parent_path+"\\"+"ADL_metadata.txt"
+	parent_path = os.path.dirname(os.path.dirname(cwd))
+	des_pos=os.path.join(parent_path,"ADL","metadata","ADL_metadata.txt")
 	fh=open(des_pos)
 	for line in fh.readlines():
 		L=string.split(line)
@@ -45,10 +48,10 @@ def	creat_file_list(author_name):
 	return file_list[1:]
 	
 if __name__=='__main__':
-	#author_name= "Blicher"
+	#type in the user's name here if authors' surnames contain æ, ø ,ë and å 
+	# e.g. author_name = "Michaëlis"
 	author_name= sys.argv[1]
 	file_list=creat_file_list(author_name)	#	get	file list 
-	
 	corpus_name=string.replace(file_list[1],".txt","_")	#	create the new folder's name using an unique id
 	corpus_name+=author_name[0]	# the form of this id is like this "numbers_numbers_charecter", eg. 13_789_B
 	
