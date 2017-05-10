184b4652de80f265fd7d41227e222cf8ff8e741f
Zehui <zy18763@nottingham.edu.cn>
2017-05-10 16:40:17 +0800

    change extract_text.py to command line argument form

--- tools/scripts_py/extract_text.py
+++ tools/scripts_py/extract_text.py
@@ -5,33 +5,38 @@ import sys,os,string
 class textHandler(ContentHandler):
     def characters(self, ch):
         sys.stdout.write(ch.encode("UTF-8"))
+##this function will take two arguments, one is the source_folder containing all the xml file
+##the other is the destination directory you use to store the output text
+def convert_to_text(source_file,des_file=os.getcwd()):
+	parser = xml.sax.make_parser()
+	handler = textHandler()
+	parser.setContentHandler(handler)
+	file_list = os.listdir(source_file)
+	print (file_list)
 
-
-parser = xml.sax.make_parser()
-handler = textHandler()
-parser.setContentHandler(handler)
-
-cwd=os.getcwd()
-parent_path = os.path.dirname(cwd)
-working_path = parent_path + "\\" +"xml_lb"+"\\"+"xml"
-file_list = os.listdir(working_path)
-
-##these three lines creates an folder called txt within xml_lb
-working_dir = parent_path + "\\" +"xml_lb"
-os.chdir(working_dir)
-os.mkdir("txt")
-##ignore if there have already existed xml_lb\txt
-for filename in file_list:
-	filepath = working_path + "\\" + filename
-	parser.parse(filepath)
-
-	temp = string.replace(filename,".xml",".txt")
-	des_file = parent_path + "\\" + "xml_lb" + "\\"+"txt"+"\\"+temp
-
-	orig_stdout = sys.stdout
-	f = file(des_file, 'w')
-	sys.stdout = f
-	parser.parse(filepath)
-	sys.stdout = orig_stdout
-	f.close()
- 
+	##these three lines creates an folder called txt within txt_output in the des_file
+	##if not specified, it will be created within the current working directory
+	os.chdir(des_file);
+	os.mkdir("txt_output");
+	des_pos=os.path.join(des_file,"txt_output")
+	os.chdir(des_pos)
+	for filename in file_list:
+		filepath = os.path.join(source_file,filename)
+		print filepath
+		parser.parse(filepath)
+		temp = string.replace(filename,".xml",".txt")
+		des_file_temp = os.path.join(des_pos,temp)
+		orig_stdout = sys.stdout
+		f = file(des_file_temp, 'w')
+		sys.stdout = f
+		parser.parse(filepath)
+		sys.stdout = orig_stdout
+		f.close()
+## use sigal quote to type in the source dir and destination dir in as command line arguments 'F:\2017\scan_modern\TEST2\GV\markup'
+if __name__=='__main__':
+	if (len(sys.argv)==3):
+		convert_to_text(sys.argv[1],sys.argv[2])
+	elif(len(sys.argv)==2):
+		convert_to_text(sys.argv[1])
+	else:
+		print("Invalid input argumnets")
